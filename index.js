a5b91b43f7a99e31ec847d1e4f9d7df7826c4208
elianisdev <elianismedina05@outlook.com>
2025-12-17 11:54:29 -0500

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Appwrite Function: Add User to Team
 // Environment variables required: APPWRITE_API_KEY, APPWRITE_PROJECT_ID
 
-import sdk from "node-appwrite";
+import * as sdk from "node-appwrite";
 
 export default async function (req, res) {
   const { userId, teamId, email, name, roles } = req.body;
